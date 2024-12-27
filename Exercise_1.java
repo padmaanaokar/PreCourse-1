63974abd5d5e415f28e15955bc84aabae02a6b63
padmaanaokar <anaokar.p@northeastern.edu>
2024-12-27 01:43:17 -0500

    Added TC and SC

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :No 
+
+// Your code here along with comments explaining your approach
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n), used stack
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :No 
+
+// Your code here along with comments explaining your approach
+
 class StackAsLinkedList { 
   
    // StackNode root; 
